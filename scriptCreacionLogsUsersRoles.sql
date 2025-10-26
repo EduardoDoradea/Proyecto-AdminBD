3a1e27e36dc7064f595ac5f4276d707e56f5b28c
EduardoDoradea <00086224@uca.edu.sv>
2025-10-25 23:41:21 -0600

    :sparkles:Agregando script con logins usuarios y roles
    falta crear los privilegios y relacionar los usuarios a roles

---
+++ scriptCreacionLogsUsersRoles.sql
@@ -0,0 +1,81 @@
+
+-- SCRIPT DE CREACION DE LOGINS - USUARIOS - ROLES
+
+--CREACION DE LOGINS (Estos se crean a nivel de servidor es decir globalmente)
+--CREACION DE TODOS LOS LOGINS 
+USE master
+
+CREATE LOGIN L_Medico
+WITH PASSWORD = 'D!@gn0s1s101!',
+CHECK_POLICY = ON,
+CHECK_EXPIRATION = OFF;
+
+CREATE LOGIN L_Paciente
+WITH PASSWORD = 'V!s1t@2025R3g',
+CHECK_POLICY = ON,
+CHECK_EXPIRATION = OFF;
+
+CREATE LOGIN L_Secretaria
+WITH PASSWORD = 'Ag3nd@$F@ctura%',
+CHECK_POLICY = ON,
+CHECK_EXPIRATION = OFF;
+
+CREATE LOGIN L_Farmaceutico
+WITH PASSWORD = 'D0s1s#M3d1c@l7',
+CHECK_POLICY = ON,
+CHECK_EXPIRATION = OFF;
+
+CREATE LOGIN L_Auxiliar_Bodega
+WITH PASSWORD = '!nv3nt@r1oC4ja$',
+CHECK_POLICY = ON,
+CHECK_EXPIRATION = OFF;
+
+CREATE LOGIN L_Enfermera
+WITH PASSWORD = 'R0ndaS@lud!99',
+CHECK_POLICY = ON,
+CHECK_EXPIRATION = OFF;
+
+CREATE LOGIN L_Ingeniero
+WITH PASSWORD = 'S!st3m@s&DBK3y',
+CHECK_POLICY = ON,
+CHECK_EXPIRATION = OFF;
+
+CREATE LOGIN L_Seguridad
+WITH PASSWORD = 'Aud!tAcc3s0#44',
+CHECK_POLICY = ON,
+CHECK_EXPIRATION = OFF;
+
+CREATE LOGIN L_Administrador
+WITH PASSWORD = 'C0ntr0lT0t@l$24',
+CHECK_POLICY = ON,
+CHECK_EXPIRATION = OFF;
+
+--CREACION DE USUARIOS EN BASE DE DATOS (Los usuarios se crean en la base de datos que se utilizara)
+USE DB_Hospital
+
+CREATE USER U_Medico FOR LOGIN L_Medico;
+CREATE USER U_Paciente FOR LOGIN L_Paciente;
+CREATE USER U_Secretaria FOR LOGIN L_Secretaria;
+CREATE USER U_Farmaceutico FOR LOGIN L_Farmaceutico;
+CREATE USER U_Auxiliar_Bodega FOR LOGIN L_Auxiliar_Bodega;
+CREATE USER U_Enfermera FOR LOGIN L_Enfermera;
+CREATE USER U_Ingeniero FOR LOGIN L_Ingeniero;
+CREATE USER U_Seguridad FOR LOGIN L_Seguridad;
+CREATE USER U_Administrador FOR LOGIN L_Administrador;
+
+--CREACION DE ROLES
+CREATE ROLE R_Medico;
+CREATE ROLE R_Paciente;
+CREATE ROLE R_Secretaria;
+CREATE ROLE R_Farmaceutico;
+CREATE ROLE R_Auxiliar_Bodega;
+CREATE ROLE R_Enfermera;
+CREATE ROLE R_Ingeniero;
+CREATE ROLE R_Seguridad;
+CREATE ROLE R_Administrador;
+
+--LE ASIGNAMOS QUE PRIVILEGIOS PUEDE REALIZAR EN QUE TABLAS AL ROLE CREADO ANTERIORMENTE
+
+--AGREGAMOS MIEMBROS A ESE ROL CREADO
+--EXEC sp_addrolemember 'rol', 'usuario'; Si quieren colocarlo con el procedimiento almacenado de caso contrario con alter role add member
+ALTER ROLE R_00086224_consultor ADD MEMBER U_00086224_consultor;
