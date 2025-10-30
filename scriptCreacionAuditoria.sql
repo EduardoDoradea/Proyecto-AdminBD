8b12fa7ff2e94719684542ccc43e97581e932c99
EduardoDoradea <00086224@uca.edu.sv>
2025-10-30 09:48:05 -0600

    feat: Agregando actualizaciones a la auditoria

--- scriptCreacionAuditoria.sql
+++ scriptCreacionAuditoria.sql
@@ -12,13 +12,41 @@ USE DB_Hospital
 
 CREATE DATABASE AUDIT SPECIFICATION AuditoriaHospital
 FOR SERVER AUDIT AuditoriaHospital
---Que tipo de audits quiero vigilar, que metodo DML o DDL a nivel base de datos o nivel de servidor quiero guardar registros 
-ADD(FAILED_LOGIN_GROUP),
-ADD(SUCESSFULL_LOGIN_GROUP),
 ADD(DATABASE_OPERATION_GROUP),
-ADD(CREATE, DROP ON SCHEMA::dbo BY PUBLIC)
-/*NOTA ME FALTA AGREGAR OTROS AUDITS, NO CREAR EL AUDIT SPECIFICATION, SI LO CREARON, LUEGO LO ELIMINAN Y VUELVEN A CREAR atte: edu:( */
+--REGISTROS DE LO QUE HA REALIZADO EL ROL DE MEDICO
+ADD(SELECT ON OBJECT::Inventario.Medicamento BY R_Medico),
+ADD(SELECT ON OBJECT::Inventario.Analgesico BY R_Medico),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Clinica.Tratamiento BY R_Medico),
+ADD(SELECT ON OBJECT::Clinica.Paciente BY R_Medico),
+ADD(SELECT ON OBJECT::Administracion.Departamento BY R_Medico),
+--Aqui se le dice registra los metodos dml del rol medico sobre el esquema completo de clinica 
+--(se podria hacer tambien para los demas esquemas)
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Clinica BY R_Medico), 
+--REGISTROS DE LO QUE HA REALIZADO EL ROL DE SECRETARIA
+ADD(SELECT ON OBJECT::Clinica.Tratamiento BY R_Secretaria),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Clinica.Cita BY R_Secretaria),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Contabilidad.Factura BY R_Secretaria),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Contabilidad.MetodoPago BY R_Secretaria),
+ADD(SELECT ON OBJECT::Clinica.Medico BY R_Secretaria),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Clinica.Paciente BY R_Secretaria),
+ADD(SELECT ON OBJECT::Administracion.Departamento BY R_Secretaria),
+--REGISTROS DE LO QUE REALIZA EL ROL DE FARMACEUTICO
+ADD(SELECT ON OBJECT::Inventario.InstrumentoMedico BY R_Farmaceutico),
+ADD(SELECT ON OBJECT::Inventario.Bodega BY R_Farmaceutico),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Inventario.Analgesico BY R_Farmaceutico),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Inventario.Medicamento BY R_Farmaceutico),
+--REGISTROS DE LO QUE REALIZA EL ROL DE AUXILIAR_BODEGA
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Inventario.InstrumentoMedico BY R_Auxiliar_Bodega),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Inventario.Bodega BY R_Auxiliar_Bodega),
+ADD(SELECT ON OBJECT::Inventario.Medicamento BY R_Auxiliar_Bodega),
+ADD(SELECT ON OBJECT::Inventario.Analgesico BY R_Auxiliar_Bodega);
+
 
 -- Activar la auditoría a nivel base de datos
 ALTER DATABASE AUDIT SPECIFICATION AuditoriaHospital 
 WITH (STATE = ON);
+
+SELECT event_time, server_principal_name, database_name, database_principal_name, schema_name, object_name, statement
+FROM sys.fn_get_audit_file('C:\SQLAudit\AuditoriaHospital\*.sqlaudit', DEFAULT, DEFAULT);
+
+
