8e25a834f095dcab4b0416ecb68089d8b53e6eac
EduardoDoradea <00086224@uca.edu.sv>
2025-10-30 10:22:43 -0600

    feat: Arreglando la auditoria

--- scriptCreacionAuditoria.sql
+++ scriptCreacionAuditoria.sql
@@ -14,32 +14,22 @@ CREATE DATABASE AUDIT SPECIFICATION AuditoriaHospital
 FOR SERVER AUDIT AuditoriaHospital
 ADD(DATABASE_OPERATION_GROUP),
 --REGISTROS DE LO QUE HA REALIZADO EL ROL DE MEDICO
-ADD(SELECT ON OBJECT::Inventario.Medicamento BY R_Medico),
-ADD(SELECT ON OBJECT::Inventario.Analgesico BY R_Medico),
-ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Clinica.Tratamiento BY R_Medico),
-ADD(SELECT ON OBJECT::Clinica.Paciente BY R_Medico),
-ADD(SELECT ON OBJECT::Administracion.Departamento BY R_Medico),
---Aqui se le dice registra los metodos dml del rol medico sobre el esquema completo de clinica 
---(se podria hacer tambien para los demas esquemas)
-ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Clinica BY R_Medico), 
+ADD(SELECT, INSERT, UPDATE ON OBJECT::Inventario.Bodega BY R_Medico),
+ADD(SELECT, INSERT, UPDATE ON OBJECT::Contabilidad.Factura BY R_Medico),
+ADD(SELECT, INSERT, UPDATE ON OBJECT::Contabilidad.MetodoPago BY R_Medico),
 --REGISTROS DE LO QUE HA REALIZADO EL ROL DE SECRETARIA
-ADD(SELECT ON OBJECT::Clinica.Tratamiento BY R_Secretaria),
-ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Clinica.Cita BY R_Secretaria),
-ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Contabilidad.Factura BY R_Secretaria),
-ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Contabilidad.MetodoPago BY R_Secretaria),
-ADD(SELECT ON OBJECT::Clinica.Medico BY R_Secretaria),
-ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Clinica.Paciente BY R_Secretaria),
-ADD(SELECT ON OBJECT::Administracion.Departamento BY R_Secretaria),
+ADD(SELECT, INSERT, UPDATE ON OBJECT::Inventario.Bodega BY R_Secretaria),
+ADD(SELECT, INSERT, UPDATE ON OBJECT::Inventario.Medicamento BY R_Secretaria),
 --REGISTROS DE LO QUE REALIZA EL ROL DE FARMACEUTICO
-ADD(SELECT ON OBJECT::Inventario.InstrumentoMedico BY R_Farmaceutico),
-ADD(SELECT ON OBJECT::Inventario.Bodega BY R_Farmaceutico),
-ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Inventario.Analgesico BY R_Farmaceutico),
-ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Inventario.Medicamento BY R_Farmaceutico),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Clinica.Tratamiento BY R_Farmaceutico),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Clinica.Paciente BY R_Farmaceutico),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Contabilidad.Factura BY R_Farmaceutico),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Contabilidad.MetodoPago BY R_Farmaceutico),
 --REGISTROS DE LO QUE REALIZA EL ROL DE AUXILIAR_BODEGA
-ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Inventario.InstrumentoMedico BY R_Auxiliar_Bodega),
-ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Inventario.Bodega BY R_Auxiliar_Bodega),
-ADD(SELECT ON OBJECT::Inventario.Medicamento BY R_Auxiliar_Bodega),
-ADD(SELECT ON OBJECT::Inventario.Analgesico BY R_Auxiliar_Bodega);
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Clinica.Tratamiento BY R_Auxiliar_Bodega),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Clinica.Paciente BY R_Auxiliar_Bodega),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Contabilidad.Factura BY R_Auxiliar_Bodega),
+ADD(SELECT, INSERT, UPDATE, DELETE ON OBJECT::Contabilidad.MetodoPago BY R_Auxiliar_Bodega);
 
 
 -- Activar la auditoría a nivel base de datos
